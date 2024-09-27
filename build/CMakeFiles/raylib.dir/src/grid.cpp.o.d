e3e41322429142e53deb1c20fa0d25935a41d9ce
Bhraman Shrestha <bhramanshrestha71@gmail.com>
2024-09-27 17:22:21 +0545

    gitignore

--- build/CMakeFiles/raylib.dir/src/grid.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/raylib.dir/src/grid.cpp.o: \
- /home/bhraman/Projects/Raylib/src/grid.cpp /usr/include/stdc-predef.h \
- /home/bhraman/Projects/Raylib/include/grid.h \
- /home/bhraman/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
--- build/CMakeFiles/raylib.dir/src/tetrimino.cpp.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/raylib.dir/src/tetrimino.cpp.o: \
- /home/bhraman/Projects/Raylib/src/tetrimino.cpp \
- /usr/include/stdc-predef.h \
- /home/bhraman/Projects/Raylib/include/tetrimino.h \
- /home/bhraman/Projects/Raylib/include/square.h \
- /home/bhraman/Projects/Raylib/include/grid.h \
- /home/bhraman/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
